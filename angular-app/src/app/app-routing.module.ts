35f8a9992a2a656dd380b95a32f8ed565ac5b271
Roshith P <roshith844@gmail.com>
2023-01-16 20:05:26 +0530

    feat: routes

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './counter/counter/counter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+//   {
+//   path: '', component: CounterComponent
+// },
+{
+  path: 'counter',
+  component: CounterComponent
+}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
