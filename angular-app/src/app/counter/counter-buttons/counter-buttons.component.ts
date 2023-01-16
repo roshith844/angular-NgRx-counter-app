5efcbada345cc92bb4bfe90b7a4c279cfed42a5f
Roshith P <roshith844@gmail.com>
2023-01-16 16:53:50 +0530

    feat: Observables

--- angular-app/src/app/counter/counter-buttons/counter-buttons.component.ts
+++ angular-app/src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -1,6 +1,7 @@
-import { Component,EventEmitter,Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store'
 import { decrement, increment, reset } from '../state/counter.action';
+import { CounterState } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-buttons',
@@ -9,7 +10,7 @@ import { decrement, increment, reset } from '../state/counter.action';
 })
 export class CounterButtonsComponent {
 
-constructor(private store : Store <{counter: {counter: number}}>){}
+constructor(private store : Store <{counter: CounterState}>){}
   onIncrement(){
 this.store.dispatch(increment())
   }
--- angular-app/src/app/counter/counter-output/counter-output.component.ts
+++ angular-app/src/app/counter/counter-output/counter-output.component.ts
@@ -1,18 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store'
+import { Observable, Subscription } from 'rxjs';
+import { CounterState } from '../state/counter.state';
+
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css']
 })
-export class CounterOutputComponent {
-  counter!: number;
-constructor(private store: Store<{counter: { counter: number}}>){
-
+export class CounterOutputComponent implements OnInit {
+  counter$ !: Observable<{counter: number}>
+constructor(private store: Store<{counter: CounterState}>){
 }
 ngOnInit(): void{
- this.store.select('counter').subscribe(data=>{
-  this.counter = data.counter
- })
+ this.counter$ = this.store.select('counter') 
 }
+
 }
